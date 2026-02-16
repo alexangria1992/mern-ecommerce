d57a0968a3069130eefa0546f731f415446f34de
Alex Angrignon <alex@example.com>
2026-02-17 12:40:17 +1300

    topbar

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Topbar from "../Layout/Topbar";
+
+const Header = () => {
+  return (
+    <header>
+      {/* Topbar */}
+      <Topbar />
+      {/* navbar */}
+      {/* cart drawer */}
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Common/Header";
+
+const UserLayout = () => {
+  return (
+    <div>
+      {/* HEader */}
+      <Header />
+      {/* Main Content */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default UserLayout;

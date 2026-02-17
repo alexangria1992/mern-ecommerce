da70428b7bf0e1c844f94c2cc97dc105d9fb2645
Alex Angrignon <alex@example.com>
2026-02-18 08:34:23 +1300

    Header Section

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Topbar from "../Layout/Topbar";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header>
+    <header className="border-b border-gray-200">
       {/* Topbar */}
       <Topbar />
       {/* navbar */}
+      <Navbar />
       {/* cart drawer */}
     </header>
   );
